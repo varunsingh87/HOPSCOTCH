d0483aeba04224e788163c3475a5d860c952ce8b
Skiwee45 <raymond.lin.05@gmail.com>
2022-09-03 14:19:17 -0400

    Login logout

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,23 @@
 import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { ConvexProvider, ConvexReactClient } from 'convex/react'
+import { ConvexProviderWithAuth0 } from "convex/react-auth0";
+import { ConvexReactClient } from 'convex/react'
 import clientConfig from "../convex/_generated/clientConfig";
+import convexConfig from "../convex.json";
+import { Login } from "../Lib/account-auth";
+
 const convex = new ConvexReactClient(clientConfig);
+const authInfo = convexConfig.authInfo[0];
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ConvexProvider client={convex}>
+    <ConvexProviderWithAuth0 
+      client={convex}
+      authInfo={authInfo}
+      loggedOut={<Login/>}>
       <Component {...pageProps} />
-    </ConvexProvider>
+    </ConvexProviderWithAuth0>
   )
 }
 
