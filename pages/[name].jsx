48c6423e799b4daefb312f024c9528410e63e533
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-09-03 14:43:56 -0400

    1:00 to 2:00

--- pages/[name].jsx
+++ pages/[name].jsx
@@ -1,16 +1,16 @@
-import { useMutation, useQuery } from "../convex/_generated/react"
+// import { useMutation, useQuery } from "../convex/_generated/react"
 
-export default function App(props) {
+// export default function App(props) {
 
-}
+// }
 
-export function getStaticPaths() {
-    const competitions = useMutation("listCompetitions")
-    return {
-        paths: [
-            competitions.map(item => {
-                return { params: item }
-            })
-        ]
-    }
-}
+// export function getStaticPaths() {
+//     const competitions = useMutation("listCompetitions")
+//     return {
+//         paths: [
+//             competitions.map(item => {
+//                 return { params: item }
+//             })
+//         ]
+//     }
+// }
