2262e1b4ded9d825afe3e1905d16fb1f00f79b66
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-09-04 04:00:03 -0400

    Uncomment static child components

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -23,42 +23,42 @@ export default function Profile() {
                     {/* <button className="mt-1 btn btn-success btn-sm" onClick={() => storeUser(bio)}>Save<Check /></button> */}
                 </div>
             </div>
-            {/* <SubmissionsList /> */}
+            <SubmissionsList />
         </div>
     );
 }
 
-// function SubmissionViewer() {
-//     return (
-//         <div class="card">
-//             <img src="https://www.aboutbritain.com/images/articles/big/medieval-knights-shutterstock-trio-92785621.jpg"
-//                 class="card-img-top"
-//                 height="100px"
-//                 width="auto"
-//                 style={{ objectFit: "cover" }} />
-//             <div class="card-body">
-//                 <h5 class="card-title">Medieval Chant</h5>
-//                 <p class="card-text">Encapsulates the spirit of the most noble and bravest knights.</p>
-//             </div>
-//             <div class="card-footer">
-//                 <small class="text-muted">Last updated {Math.round(Math.random() * 5)} mins ago</small>
-//             </div>
-//         </div>
-//     );
-// }
+function SubmissionViewer() {
+    return (
+        <div class="card">
+            <img src="https://www.aboutbritain.com/images/articles/big/medieval-knights-shutterstock-trio-92785621.jpg"
+                class="card-img-top"
+                height="100px"
+                width="auto"
+                style={{ objectFit: "cover" }} />
+            <div class="card-body">
+                <h5 class="card-title">Medieval Chant</h5>
+                <p class="card-text">Encapsulates the spirit of the most noble and bravest knights.</p>
+            </div>
+            <div class="card-footer">
+                <small class="text-muted">Last updated {Math.round(Math.random() * 5)} mins ago</small>
+            </div>
+        </div>
+    );
+}
 
-// function SubmissionsList() {
-//     return (
-//         <div class="row row-cols-1 row-cols-md-3 g-4">
-//             <div class="col">
-//                 <SubmissionViewer />
-//             </div>
-//             <div class="col">
-//                 <SubmissionViewer />
-//             </div>
-//             <div class="col">
-//                 <SubmissionViewer />
-//             </div>
-//         </div>
-//     )
-// }
+function SubmissionsList() {
+    return (
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <SubmissionViewer />
+            </div>
+            <div class="col">
+                <SubmissionViewer />
+            </div>
+            <div class="col">
+                <SubmissionViewer />
+            </div>
+        </div>
+    )
+}
