13744eb6ef91336b8a2e7b92dd4edc1a4495405d
adityabilawar <23ab1107@wwprsd.org>
2022-09-04 01:09:38 -0400

    updated navbar

--- Components/layout.tsx
+++ Components/layout.tsx
@@ -1,10 +1,13 @@
-import { Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from "reactstrap";
+import { Container, Nav, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink ,Collapse,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem} from "reactstrap";
 import { Logout } from "../lib/account-auth"
 import { useEffect, useState } from "react"
 import { Document, Id } from "../convex/_generated/dataModel";
 import { useMutation } from "../convex/_generated/react";
 import Link from "next/link";
-
 // Render a chat message.
 export function Layout(props: any) {
   const [userId, setUserId] = useState<Id<"users"> | null>(null);
@@ -26,14 +29,22 @@ export function Layout(props: any) {
   return (
     <div>
       <Navbar>
-        <NavbarBrand href="/">Musathon</NavbarBrand>
-        <Nav>
-            <Link href="/">Browse Musathons</Link>
-            <Link href="/new-competition">Host a Musathon</Link>
-        </Nav>
+        <NavbarBrand href="/" ><h1><img alt='logo' height={120} src="https://media.discordapp.net/attachments/896181221200113735/1015839374727979019/logo.PNG"></img>Musathon</h1></NavbarBrand>
+        <Nav className="me-auto" navbar> 
+        <NavItem>
+                <NavLink href="/">Browse Musathons</NavLink>
+              </NavItem>
+              </Nav>
+              <Nav  navbar>
+              <NavItem className="justify-content-center">
+                <NavLink href="/new-competition">Host a Musathon</NavLink>
+              </NavItem>
+              </Nav>
         <Logout />
+        
       </Navbar>
-      <Container className="mt-2">{props.children}</Container>
+      <Container>{props.children}</Container>
+      
     </div>
   );
 }
