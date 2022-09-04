6415b6120f8ae343d6e8d57cbc3209071a913ad2
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-09-04 04:08:11 -0400

    Uncomment useEffect

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -4,14 +4,12 @@ import { useMutation } from "../convex/_generated/react";
 
 export default function Profile() {
     const userbio = useQuery("getUserBio");
-    const [bio, setBio] = useState(
-        "Sample Bio: I am very enthusiastic about Hackathons, and since I also love the Piano and Composing, I was very happy when I found this fully functional and bug free site that hosts music competitions.");
+    const [bio, setBio] = useState("Loading Bio...");
     const storeUser = useMutation("storeUser");
 
-    // useEffect(() => {
-    //     console.log(userbio)
-    //     setBio(userbio)
-    // }, [])
+    useEffect(() => {
+        setBio(userbio)
+    }, [])
 
     return (
         <div>
