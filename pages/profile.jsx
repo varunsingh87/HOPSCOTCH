c79084aee50d867f93c34b4ce405945147c0ff9f
Varun <31084854+varunsingh87@users.noreply.github.com>
2022-09-04 04:01:49 -0400

    uncomment storeUser

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -6,7 +6,7 @@ export default function Profile() {
     // const userbio = useQuery("getUserBio");
     const [bio, setBio] = useState(
         "Sample Bio: I am very enthusiastic about Hackathons, and since I also love the Piano and Composing, I was very happy when I found this fully functional and bug free site that hosts music competitions.");
-    // const storeUser = useMutation("storeUser");
+    const storeUser = useMutation("storeUser");
 
     // useEffect(() => {
     //     console.log(userbio)
